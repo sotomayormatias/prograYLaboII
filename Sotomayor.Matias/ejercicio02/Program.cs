89d560d2e833a5b97b273777ceeb27c347267bf3
sotomayormatias <matisoto_77@hotmail.com>
2016-08-24 21:03:06 -0300

    se agrega ejercicio 02

---
+++ Sotomayor.Matias/ejercicio02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese un numero");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero < 0) {
+                Console.WriteLine("Ingrese un numero POSITIVO");
+                numero = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("El cuadrado es: {0} y el cubo es: {1}", Math.Pow(numero, 2), Math.Pow(numero, 3));
+
+            Console.ReadKey();
+        }
+    }
+}
