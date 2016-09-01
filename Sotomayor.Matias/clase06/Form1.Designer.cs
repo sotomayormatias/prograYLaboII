252cce32b5ed68f9fc12d884eda812cb0d7161ee
sotomayormatias <matisoto_77@hotmail.com>
2016-09-01 20:35:50 -0300

    Parte de clase 06

---
+++ Sotomayor.Matias/clase06/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace clase06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.AutoSize = true;
+            this.lblSaludo.Location = new System.Drawing.Point(146, 49);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(0, 16);
+            this.lblSaludo.TabIndex = 0;
+            this.lblSaludo.UseCompatibleTextRendering = true;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(104, 177);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "&Pulsar aquí";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.lblSaludo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ Sotomayor.Matias/clase06/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clase06
+{
+    public partial class Form1 : Form
+    {
+        private int cantClicks = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Bienvenidos!! " + DateTime.Now;
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            this.lblSaludo.Text = "Hola mundo!";
+            switch (cantClicks) { 
+                case 1:
+                    this.BackColor = Color.Purple;
+                    this.cantClicks++;
+                    break;
+                case 2:
+                    this.BackColor = Color.Red;
+                    this.cantClicks++;
+                    break;
+                case 3:
+                    this.BackColor = Color.Yellow;
+                    this.cantClicks++;
+                    break;
+                case 4:
+                    this.BackColor = Color.Green;
+                    this.cantClicks = 1;
+                    break;
+            }
+        }
+
+    }
+}
