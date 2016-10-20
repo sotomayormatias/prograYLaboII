f3e015abdb3e82a5671cb46ce8135283ad575596
sotomayormatias <matisoto_77@hotmail.com>
2016-10-19 21:27:00 -0300

    clase 13

--- Sotomayor.Matias/clase13/Program.cs
+++ Sotomayor.Matias/clase13/Program.cs
@@ -11,13 +11,21 @@ namespace clase13
     {
         static void Main(string[] args)
         {
-            //****************AUTOS****************//
-
+            Cocina c1 = new Cocina(111, 12300, false);
+            Cocina c2 = new Cocina(112, 15000, true);
+            Cocina c3 = new Cocina(113, 5600, false);
             Auto a1 = new Auto("Rojo", "Ferrari");
             Auto a2 = new Auto("Amarillo", "Porche");
             Auto a3 = new Auto("Negro", "BMW");
             Auto a4 = new Auto("Verde", "Ford");
-            DepositoDeAutos da = new DepositoDeAutos(3);
+            Deposito<Cocina> dc = new Deposito<Cocina>(5);
+            Deposito<Auto> da = new Deposito<Auto>(3);
+            dc.Agregar(c1);
+            dc.Agregar(c2);
+            if (!(dc + c3))
+            {
+                Console.WriteLine("No se pudo agregar el item!!!");
+            }
             if ((da + a1))
             {
                 Console.WriteLine("Se ha agregado el item!!!");
@@ -28,34 +36,20 @@ namespace clase13
             {
                 Console.WriteLine("No se pudo agregar el item!!!");
             }
-            Console.WriteLine(da);
-            da.Remover(a2);
-            if (!(da - a4))
-            {
-                Console.WriteLine("No se pudo remover el item!!!");
-            }
-            Console.WriteLine(da);
-            Console.ReadLine();
-
-            //****************COCINAS****************//
-
-            Cocina c1 = new Cocina(111, 12300, false);
-            Cocina c2 = new Cocina(112, 15000, true);
-            Cocina c3 = new Cocina(113, 5600, false);
-            DepositoDeCocinas dc = new DepositoDeCocinas(5);
-            dc.Agregar(c1);
-            dc.Agregar(c2);
-            if (!(dc + c3))
-            {
-                Console.WriteLine("No se pudo agregar el item!!!");
-            }
             Console.WriteLine(dc);
+            Console.WriteLine(da);
             dc.Remover(c2);
             if (!(dc - c2))
             {
                 Console.WriteLine("No se pudo remover el item!!!");
             }
+            da.Remover(a2);
+            if (!(da - a4))
+            {
+                Console.WriteLine("No se pudo remover el item!!!");
+            }
             Console.WriteLine(dc);
+            Console.WriteLine(da);
             Console.ReadLine();
         }
     }
---
+++ Sotomayor.Matias/clase13_ClassLibrary/Deposito.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase13_ClassLibrary
+{
+    public class Deposito<T>
+    {
+        private int _capacidadMaxima;
+        private List<T> _lista;
+
+        public Deposito(int capacidad)
+        {
+            this._capacidadMaxima = capacidad;
+            this._lista = new List<T>();
+        }
+
+        public static bool operator +(Deposito<T> depo, T unItem)
+        {
+            if (depo._lista.Count() < depo._capacidadMaxima)
+            {
+                depo._lista.Add(unItem);
+                return true;
+            }
+
+            return false;
+        }
+
+        private int GetIndice(T unItem)
+        {
+            for (int i = 0; i < this._lista.Count(); i++)
+            {
+                if (this._lista[i].Equals(unItem))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public static bool operator -(Deposito<T> depo, T unItem)
+        {
+            int indice = depo.GetIndice(unItem);
+
+            if (indice > 0)
+            {
+                depo._lista.RemoveAt(indice);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Agregar(T unItem) 
+        {
+            return this + unItem;
+        }
+
+        public bool Remover(T unItem)
+        {
+            return this - unItem;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Capacidad Maxima: ");
+            sb.Append(this._capacidadMaxima + "\n");
+            sb.Append("Listado de " + typeof(T).Name + ": \n");
+
+            foreach (T item in this._lista)
+            {
+                sb.Append(item.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
