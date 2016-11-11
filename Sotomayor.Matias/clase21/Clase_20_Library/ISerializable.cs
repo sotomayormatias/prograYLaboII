ea6b18d9123d7f604c7405767f1b5c4bb6dfed17
sotomayormatias <matisoto_77@hotmail.com>
2016-11-10 21:45:14 -0300

    clase 21 - manejo de base de datos

---
+++ Sotomayor.Matias/clase21/Clase_20_Library/ISerializable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public interface ISerializable<T>
+    {
+        bool guardar();
+        bool modificar(int id);
+        bool leer(int id);
+        bool eliminar(int id);
+    }
+}
